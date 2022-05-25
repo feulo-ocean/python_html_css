4091c121279a30202543d5083384f38123a96e61
feulo-ocean <guilherme.feulo@oceanbrasil.com>
2022-05-25 00:30:26 +0000

    aula2

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+
+DATABASE = "banco.bd"
+SECRET_KEY = "chave"
 
 app = Flask("Hello")
+app.config.from_object(__name__)
 
-@app.route("/hello")
-def hello():
-    return render_template("hello.html")
+def conecta_bd():
+    return sqlite3.connect(DATABASE)
+
+@app.before_request
+def antes_requisicao():
+    g.bd = conecta_bd()
+
+@app.teardown_request
+def depois_requisicao(e):
+    g.bd.close()
+
+@app.route("/")
+def exibir_entradas():
+    sql = "SELECT titulo, texto, criado_em FROM entradas ORDER BY id DESC;"
+    cur = g.bd.execute(sql)
+    entradas = []
+    for titulo, texto, criado_em in cur.fetchall():
+        entradas.append({"titulo": titulo, "texto": texto, "criado_em": criado_em})
+    return render_template("layout.html", entradas=entradas)
---
+++ esquema.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS entradas;
+CREATE TABLE entradas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo STRING NOT NULL,
+    texto STRING NOT NULL,
+    criado_em DATETIME DEFAULT NOW
+);
